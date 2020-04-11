a515f9a7d725352094b144af7aa49ecb6d180ddc
URIS2019_ITjevci <lukasaric.saric997@gmail.com>
2020-04-11 17:41:12 +0200

    Updates

---
+++ precache-manifest.fed95b5ebb22a0bfb415f398e7cc0a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813ad8dbe683bf548321a6648f386797",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "2ae2c638159c4e9e67a3",
+    "url": "/robots-rolodex/static/css/main.f7013f8c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/robots-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae2c638159c4e9e67a3",
+    "url": "/robots-rolodex/static/js/main.3a6a9111.chunk.js"
+  },
+  {
+    "revision": "f249aee8820cb360f70a",
+    "url": "/robots-rolodex/static/js/runtime-main.aa66a150.js"
+  }
+]);
